9c33c0145efb1be2f19250d7050b71e864bc8e7b
jaythot1 <jaythot1@in.ibm.com>
2019-01-09 20:28:14 +0530

    initialising collections

--- src/main/java/com/stackroute/spring/Triangle.java
+++ src/main/java/com/stackroute/spring/Triangle.java
@@ -1,65 +1,21 @@
 package com.stackroute.spring;
 
+import java.util.List;
+
 public class Triangle {
-//    public String getType() {
-//        return type;
-//    }
-//    public Triangle(String type){
-//        this.type=type;
-//    }
-//
-//    public Triangle(int height){
-//        this.height=height;
-//    }
-////    public void setType(String type) {
-////        this.type = type;
-////    }
-//
-//    private String type;
-//    private int height;
-//
-//    public Triangle(String type, int height) {
-//        this.type = type;
-//        this.height = height;
-//    }
-//
-//    public int getHeight() {
-//        return height;
-//    }
+    List<Point> points;
 
-    private Point pointA;
-    private Point pointB;
-    private Point pointC;
-
-
-
-    public Point getPointA() {
-        return pointA;
+    public List<Point> getPoints() {
+        return points;
     }
 
-    public void setPointA(Point pointA) {
-        this.pointA = pointA;
+    public void setPoints(List<Point> points) {
+        this.points = points;
     }
 
-    public Point getPointB() {
-        return pointB;
-    }
-
-    public void setPointB(Point pointB) {
-        this.pointB = pointB;
-    }
-
-    public Point getPointC() {
-        return pointC;
-    }
-
-    public void setPointC(Point pointC) {
-        this.pointC = pointC;
-    }
-    public void draw(){
-//        System.out.println(getType()+"Triangle drawn of height:"+getHeight());
-        System.out.println("Point A=("+getPointA().getX()+","+getPointA().getY()+")");
-        System.out.println("Point B=("+getPointB().getX()+","+getPointB().getY()+")");
-        System.out.println("Point C=("+getPointC().getX()+","+getPointC().getY()+")");
+    public void draw() {
+        for (Point point : points) {
+            System.out.println("Point C=(" + point.getX() + "," + point.getY() + ")");
+        }
     }
 }
